49e677c2711311b9e3474be96595edcfcb72324d
md0707 <ppuggy1@gmail.com>
2018-07-10 21:21:30 +0000

    automatically backed up by learn

--- two_step_and_repeat.rb
+++ two_step_and_repeat.rb
@@ -10,8 +10,7 @@ sleep(1)
 end
 
 def a_few_more_steps
-  # Write a loop that outputs the first two sets of steps in the Two-Step
-end
+  
 
 def how_many_steps?
   # Write a loop that fulfills all criteria from Part 3 of the README.md
